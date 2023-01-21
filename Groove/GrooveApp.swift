73aa05cde52c63b83856b83ed18a8790b23a6551
Jordan Guerguiev <jordan.guerguiev@me.com>
2023-01-21 17:17:09 -0500

    Initial Commit

---
+++ Groove/GrooveApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrooveApp.swift
+//  Groove
+//
+//  Created by Jordan Guerguiev on 2023-01-21.
+//
+
+import SwiftUI
+
+@main
+struct GrooveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
